52314f5a20ea31696bb69d82d0d9512ec6987c72
liansun96 <toetetaung3296@gmail.com>
2024-03-22 23:23:20 +0630

    http_read html file

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log("Express Tutorial");
+const http = require("http");
+const { readFileSync } = require("fs");
+
+const homePage = readFileSync("./index.html");
+
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  if (url === "/") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write(homePage);
+    res.end();
+  } else if (url === "/about") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write("<h1>This is About Dude!</h1>");
+    res.end();
+  } else {
+    res.writeHead(400, { "content-type": "text/html" });
+    res.write("<h1>Page Not Found</h1>");
+    res.end();
+  }
+});
+
+server.listen(5000);
